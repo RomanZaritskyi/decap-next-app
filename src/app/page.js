8ae3c7d34a9f7e118595db9513ef56201d4c53af
Roman <romanzaritskyi@gmail.com>
2023-07-20 22:12:46 +0300

    ww

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { attributes, react as HomeComponent } from "../content/home.md";
+import { attributes, react as HomeComponent } from "../../content/home.md";
 
 export default function Home() {
   let { title, cats } = attributes;
